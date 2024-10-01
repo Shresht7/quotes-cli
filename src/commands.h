a75ccbc21e4284f2a15a5ff70f0622cd78d97fa4
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-10-02 00:10:31 +0530

    Update docstrings

--- src/commands.h
+++ src/commands.h
@@ -3,8 +3,12 @@
 
 #include "config.h"
 
+/// @brief Displays a random quote from the CSV file.
+/// @param cfg The configuration object containing settings for displaying the quote.
 void show_random_quote(Config &cfg);
 
+/// @brief Retrieves and displays a specific quote by index from the CSV file.
+/// @param cfg The configuration object containing settings for displaying the quote.
 void get_quote(Config &cfg);
 
 #endif // COMMANDS_H
