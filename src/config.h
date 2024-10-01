6af6614cdafa0658c8ca523fac894f7cd3e659ff
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2024-10-01 23:24:52 +0530

    Refactor `config.h` file

--- src/config.h
+++ src/config.h
@@ -13,21 +13,31 @@ void print_help();
 class Config
 {
 private:
+    /// @brief The vector of positional arguments
     std::vector<std::string> args;
 
 public:
+    /// @brief The path to the source file containing the quotes
     std::string filepath;
 
+    /// @brief The style of borders
     std::string border;
+
+    /// @brief The border color
     std::string color;
+
+    /// @brief The number of blank lines before and after the output
     unsigned int margin;
 
+    /// @brief Indicates if the output should be plain and simple
     bool plain;
 
     // Default Constructor
     Config();
 
-    // Method to parse command-line arguments
+    /// @brief Parses the command-line arguments
+    /// @param argc The total count of arguments
+    /// @param argv The vector containing the command-line arguments
     int parse_arguments(int argc, char *argv[]);
 
     /// @brief Retrieve the positional argument at the given index
@@ -36,7 +46,6 @@ public:
     std::optional<std::string> get_positional_argument(unsigned int n);
 
     std::string format_quote(Quote q);
-
     std::string format_styled_quote(const Quote &quote);
 };
 
